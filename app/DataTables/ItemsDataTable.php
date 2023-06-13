ba8c1c1f402d5269b9c2b45240f6acbbffa62d3b
anwisu <cabauatan.jianellaguia@gmail.com>
2023-06-13 22:04:38 +0800

    Update ItemsDataTable.php

--- app/DataTables/ItemsDataTable.php
+++ app/DataTables/ItemsDataTable.php
@@ -103,7 +103,7 @@ class ItemsDataTable extends DataTable
      *
      * @return string
      */
-    protected function filename()
+    protected function filename(): string
     {
         return 'Items_' . date('YmdHis');
     }
